759930defed3b031791d137965885cc86e3997de
Marta Khomytska <martakhomytska@gmail.com>
2025-10-22 16:04:56 +0300

    Refactored the volunteer section to introduce VolunteerActionCardComponent and refactored VolunteerActionComponent to manage the collection of these cards.

--- component/client/VolunteerActionComponent.ts
+++ component/client/VolunteerActionComponent.ts
@@ -1,28 +1,42 @@
-// component/client/VolunteerActionComponent.ts
+import { Locator, Page } from '@playwright/test';
+import { VolunteerActionCardComponent } from './VolunteerActionCardComponent';
+export class VolunteerActionComponent {
+    private readonly sectionCards: Locator; 
+    private readonly offerHelpButton: Locator;
 
-import {Locator, Page} from '@playwright/test';
-import {BaseComponent} from "./BaseComponent";
-
-
-export class VolunteerActionComponent extends BaseComponent {
-    sectionCards: Locator;
-    offerHelpButton: Locator;
-
-    constructor(page: Page, parentLocator: Locator) {
-        super(page, parentLocator);
-
+    constructor(private page: Page) {
+        const parentContainer = 'div.MuiBox-root.css-1fesufy';
         this.sectionCards = this.page.locator('div.MuiBox-root.css-1fesufy > div.MuiBox-root');
-
+        
         this.offerHelpButton = this.page.locator('body > div.MuiBox-root.css-swu6uj > div > div > div.MuiBox-root.css-c1nekw > div.MuiBox-root.css-1fesufy > a > div > div.MuiBox-root.css-1vx2ffw > p');
     }
 
+
+
     async isMainTitleVisible(): Promise<boolean> {
-        return this.page.locator('h2', {hasText: 'ДОПОМОГТИ СПРАВАМИ'}).isVisible();
+        return this.page.getByTestId("title-icon").isVisible();
     }
+
+
+
+    async getCards(): Promise<VolunteerActionCardComponent[]> {
+        const count = await this.sectionCards.count();
+        const cards: VolunteerActionCardComponent[] = [];
+
+        for (let i = 0; i < count; i++) {
+            const cardRoot = this.sectionCards.nth(i);
+            cards.push(new VolunteerActionCardComponent(cardRoot));
+        }
+        return cards;
+    }
+    
+
 
     async getNumberOfDescriptionSections(): Promise<number> {
         return this.sectionCards.count();
     }
+
+
 
     async clickOfferHelpButton(): Promise<void> {
         await this.offerHelpButton.click();
--- page/client/SupportUsPage.ts
+++ page/client/SupportUsPage.ts
@@ -4,26 +4,13 @@ import {VolunteerActionComponent} from '../../component/client/VolunteerActionCo
 
 export class SupportUsPage extends BasePage {
 
-    translationAssistance: VolunteerActionComponent;
-    eventOrganizationAssistance: VolunteerActionComponent;
-    socialMediaManagement: VolunteerActionComponent;
+    public volunteerAction: VolunteerActionComponent;
 
     constructor(page: Page) {
         super(page);
-        this.translationAssistance = new VolunteerActionComponent(
-            this.page,
-            this.page.locator('xpath=/html/body/div[2]/div/div/div[3]/div[2]/div[1]')
-        );
-        this.eventOrganizationAssistance = new VolunteerActionComponent(
-            this.page,
-            this.page.locator('xpath=/html/body/div[2]/div/div/div[3]/div[2]/div[2]')
-        );
-        this.socialMediaManagement = new VolunteerActionComponent(
-            this.page,
-            this.page.locator('xpath=/html/body/div[2]/div/div/div[3]/div[2]/div[3]')
-        );
+        this.volunteerAction = new VolunteerActionComponent(page);
     }
-
+    
     async visit(): Promise<void> {
         await this.goto('/support-us');
     }
