296ef8e86d6d1acc28d58e64d19c9731d55bf7c3
Liubomyr Halamaha <galamagal86@gmail.com>
2025-10-22 12:07:37 +0300

    update

--- page/client/SupportUsPage.ts
+++ page/client/SupportUsPage.ts
@@ -1,17 +1,31 @@
-import { Page } from '@playwright/test';
-import { BasePage } from './BasePage';
-import { VolunteerActionComponent } from '../../component/client/VolunteerActionComponent';
+import {Page} from '@playwright/test';
+import {BasePage} from './BasePage';
+import {VolunteerActionComponent} from '../../component/client/VolunteerActionComponent';
 
 export class SupportUsPage extends BasePage {
-  public readonly volunteerAction: VolunteerActionComponent;
 
-  constructor(page: Page) {
-    super(page);
-    this.volunteerAction = new VolunteerActionComponent(page);
-  }
+    translationAssistance: VolunteerActionComponent;
+    eventOrganizationAssistance: VolunteerActionComponent;
+    socialMediaManagement: VolunteerActionComponent;
 
-  async visit(): Promise<void> {
-    await this.goto('/support-us');
-  }
+    constructor(page: Page) {
+        super(page);
+        this.translationAssistance = new VolunteerActionComponent(
+            this.page,
+            this.page.locator('xpath=/html/body/div[2]/div/div/div[3]/div[2]/div[1]')
+        );
+        this.eventOrganizationAssistance = new VolunteerActionComponent(
+            this.page,
+            this.page.locator('xpath=/html/body/div[2]/div/div/div[3]/div[2]/div[2]')
+        );
+        this.socialMediaManagement = new VolunteerActionComponent(
+            this.page,
+            this.page.locator('xpath=/html/body/div[2]/div/div/div[3]/div[2]/div[3]')
+        );
+    }
+
+    async visit(): Promise<void> {
+        await this.goto('/support-us');
+    }
 }
 
