0980eb6cdeca6633411aa955c4c8b78fae966a65
Marta Khomytska <martakhomytska@gmail.com>
2025-10-22 17:40:00 +0300

    Add QuickDonationComponent and integrate into SupportUsPage

--- page/client/SupportUsPage.ts
+++ page/client/SupportUsPage.ts
@@ -1,9 +1,14 @@
 import { Page } from '@playwright/test';
 import { BasePage } from './BasePage';
+import { QuickDonationComponent } from '../../component/client/QuickDonationComponent';
 
 export class SupportUsPage extends BasePage {
+
+  public quickDonation: QuickDonationComponent;
+
   constructor(page: Page) {
     super(page);
+    this.quickDonation = new QuickDonationComponent(page);
   }
 
   async visit(): Promise<void> {
