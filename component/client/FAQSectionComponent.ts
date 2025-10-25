9b78552ac940f80c760ec21e27a203a600b4ba59
Marta Khomytska <martakhomytska@gmail.com>
2025-10-25 15:42:22 +0300

    Implementation of FAQSectionComponent

--- component/client/FAQSectionComponent.ts
+++ component/client/FAQSectionComponent.ts
@@ -2,27 +2,30 @@ import { Locator } from "@playwright/test";
 export class FAQSectionComponent{
     private root: Locator;
     private questionTitle: Locator;
-    private questionAnswer: Locator;
     private answerContainer: Locator;
 
     constructor(root: Locator){
         this.root = root;
         this.answerContainer = this.root.locator('#Faq-content');
-        this.questionAnswer = this.answerContainer.locator('p').first();
         this.questionTitle = this.root.locator('#Faq-header');
         
     }
 
-    async expandQuestionSection(): Promise<void>{
+    async expand(): Promise<void>{
         await this.questionTitle.click();
+    }
+    async isExpanded(): Promise<boolean> {
+        return this.questionTitle.getAttribute('aria-expanded').then(attr => attr === 'true');
     }
 
     async getAnswerText(): Promise<string | null> {
-        await this.questionAnswer.waitFor({state : "visible"});
-        return this.questionAnswer.textContent();
+        const textLocator = this.answerContainer.locator('p').first();
+        
+        await textLocator.waitFor({state : "visible"}); 
+        return textLocator.textContent();
     }
 
-    async getTitleText(): Promise<string|null> {
+    async getQuestionTitleText(): Promise<string|null> {
         return this.questionTitle.textContent();
     }
 
