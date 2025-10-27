02de10524c97b901fcb7acee84c4a28423397a54
Liubomyr Halamaha <galamagal86@gmail.com>
2025-10-27 11:54:45 +0200

    fix

--- component/client/FAQSectionComponent.ts
+++ component/client/FAQSectionComponent.ts
@@ -1,33 +1,34 @@
-import { Locator } from "@playwright/test";
-export class FAQSectionComponent{
+import {Locator} from "@playwright/test";
+
+export class FAQSectionComponent {
     private root: Locator;
     private questionTitle: Locator;
     private answerContainer: Locator;
 
-    constructor(root: Locator){
+    constructor(root: Locator) {
         this.root = root;
         this.answerContainer = this.root.locator('#Faq-content');
         this.questionTitle = this.root.locator('#Faq-header');
-        
+
     }
 
-    async expand(): Promise<void>{
+    async expand(): Promise<void> {
         await this.questionTitle.click();
     }
+
     async isExpanded(): Promise<boolean> {
-        return this.questionTitle.getAttribute('aria-expanded').then(attr => attr === 'true');
+        const attr = await this.questionTitle.getAttribute('aria-expanded');
+        return attr === 'true';
     }
 
     async getAnswerText(): Promise<string | null> {
         const textLocator = this.answerContainer.locator('p').first();
-        
-        await textLocator.waitFor({state : "visible"}); 
+
+        await textLocator.waitFor({state: "visible"});
         return textLocator.textContent();
     }
 
-    async getQuestionTitleText(): Promise<string|null> {
+    async getQuestionTitleText(): Promise<string | null> {
         return this.questionTitle.textContent();
     }
-
-
 }
