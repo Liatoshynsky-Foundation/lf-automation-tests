2017c166eb3dcd27616b7bd3f44b9763da3d24ce
Marta Khomytska <martakhomytska@gmail.com>
2025-10-29 11:16:51 +0200

    Implement FAQComponent

--- component/client/FAQSectionComponent.ts
+++ component/client/FAQSectionComponent.ts
@@ -11,7 +11,7 @@ export class FAQSectionComponent {
         
     }
 
-    async expand(): Promise<void> {
+    async clickQuestionTitle(): Promise<void> {
         await this.questionTitle.click();
     }
     async isExpanded(): Promise<boolean> {
